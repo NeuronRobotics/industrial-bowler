47c89526faf7da1c504972c8828b86c5b21334a1
kharrington <kharrington@neuronrobotics.com>
2014-05-20 19:43:26 -0400

    fixed hang-at-grub bug.

--- application/TanuryGoldLogger/pushJar.sh
+++ application/TanuryGoldLogger/pushJar.sh
@@ -15,15 +15,20 @@ do
 	
 	scp target/TanuryDeviceServer.jar root@$VAR:/
 
+	# udev rules for the dyio
 	scp 80-neuronrobotics.rules root@$VAR:/etc/udev/rules.d/
+	
+	# keepalive scripts
 	scp tanuryCron.sh root@$VAR:/
 	scp tanuryCronStatus.sh root@$VAR:/
 	
+	# update boot process and bootloader to prevent hangs on boot
 	scp rcS root@$VAR:/etc/default/
 	
+	#this won't work unless you run boot-repair
 	scp grub root@$VAR:/etc/default/
-	ssh root@$VAR 'touch /boot/grub/menu.lst'
-	ssh root@$VAR 'update-grub'
+	#ssh root@$VAR 'touch /boot/grub/menu.lst'
+	ssh root@$VAR 'update-grub2'
 	
 	#ssh root@$VAR 'ntptime;rpi-update'
 	#ssh root@$VAR 'mount -o remount,ro /'
