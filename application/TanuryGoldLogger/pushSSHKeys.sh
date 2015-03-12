caa0b7f45cc17169839637567de640600db435db
kharrington <kharrington@neuronrobotics.com>
2015-03-12 13:12:30 -0400

    using root to push the keys

--- application/TanuryGoldLogger/pushSSHKeys.sh
+++ application/TanuryGoldLogger/pushSSHKeys.sh
@@ -2,7 +2,7 @@ for VAR in $(java -jar FindDevices.jar)
 do
 	echo Found $VAR
 	echo Uploading keys to $VAR;
-	cat ~/.ssh/id_rsa.pub | ssh tanury@$VAR 'cat >> .ssh/authorized_keys'
+	cat ~/.ssh/id_rsa.pub | ssh root@$VAR 'cat >> /home/tanury/.ssh/authorized_keys'
 	cat ~/.ssh/id_rsa.pub | ssh root@$VAR 'cat >> .ssh/authorized_keys'
 	#scp ~/.ssh/id_dsa.pub tanury@$VAR:.ssh/authorized_keys2
 	#scp ~/.ssh/id_dsa.pub root@$VAR:.ssh/authorized_keys2
